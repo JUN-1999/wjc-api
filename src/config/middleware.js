38498c34067bbfb1f65a6057448e56333f7938be
JUN-1999 <1173943332@qq.com>
2022-09-06 22:09:18 +0800

    跨域 =wjc

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,7 +1,22 @@
 const path = require('path');
 const isDev = think.env === 'development';
-
+const kcors = require('kcors');
 module.exports = [
+  {
+    handle: 'trace', // trace 中间件最好放在最前面，否则在 trace 之前的错误没办法 catch 到。
+    enable: !think.isCli,
+    options: {
+      debug: isDev
+    }
+  },
+  {
+    handle: kcors, // 全局处理跨域，所有请求都会允许跨域，如果想要进行相关配置请看kcors文档进行配置
+    options: {
+      origin: '*',
+      credentials: true,
+      allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+    }
+  },
   {
     handle: 'meta',
     options: {
--- src/controller/admin/user.js
+++ src/controller/admin/user.js
@@ -1,10 +1,10 @@
 const Base = require('../base.js');
 
 module.exports = class extends Base {
- async getuserAction() {
-    let model = this.model('user');
-    let data = await model.select();
-      return this.success(data)
+  async getuserAction() {
+    const model = this.model('user');
+    const data = await model.select();
+    return this.success(data);
     // return this.display();
   }
 };
