836a3adb8f17abe34e6063a1c6ddacb95ffb9ac8
JUN-1999 <1173943332@qq.com>
2022-10-25 13:02:31 +0800

    1025 和小程序消息互通 =wjc

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,7 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   indexAction() {
-      return this.success('Hello World')
+    return this.success('Hello World');
     // return this.display();
   }
 };
