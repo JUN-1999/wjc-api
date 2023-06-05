6aa366dbac6f7265a96a1c8f2fcb252f4e28aff0
JUN-1999 <1173943332@qq.com>
2023-06-05 18:12:08 +0800

    评论上传视频图片功能 =wjc

---
+++ www/static/assets/IndexView-53049d56.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-652e1b4d.js";const o={};function r(t,a){return c(),n("div",null,"Game")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ www/static/assets/file-6c095e46.js
@@ -0,0 +1,1 @@
+import{R as e}from"./index-652e1b4d.js";const r=t=>e("/file/qiniu/uploadIMG",{method:"post",data:t,headers:{"Content-Type":"multipart/form-data"}});export{r as u};
