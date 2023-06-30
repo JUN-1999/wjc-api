b9d27811b7be81f97d27dafeca2c9e7560f97d10
JUN-1999 <1173943332@qq.com>
2023-06-30 23:52:10 +0800

    0630 用户列表

--- src/controller/websocket.js
+++ src/controller/websocket.js
@@ -1,6 +1,8 @@
 require('./global');
 const webSocketList = {
-  chatRoom: new Map()
+  chatRoom: new Map(), // 聊天室websocket列表
+  chatRoomUser: new Map(), // 聊天室用户列表
+  messageList: new Map() // 聊天消息列表
 };
 module.exports = class extends think.Controller {
   // eslint-disable-next-line no-useless-constructor
@@ -28,19 +30,60 @@ module.exports = class extends think.Controller {
 
   // 聊天室 - 加入房间
   async joinChatRoomAction() {
-    if (!webSocketList.chatRoom.has(this.wsData.data.useruuid)) {
-      webSocketList.chatRoom.set(this.wsData.data.useruuid, this.websocket);
+    // 添加文章的列表
+    if (!webSocketList.chatRoom.has(this.wsData.data.articleuuid)) {
+      webSocketList.chatRoom.set(this.wsData.data.articleuuid, new Map());
+      webSocketList.chatRoomUser.set(this.wsData.data.articleuuid, []);
+    }
+
+    // 获取文章id对应的聊天室内容
+
+    const chatRoom = webSocketList.chatRoom.get(this.wsData.data.articleuuid);
+    const chatRoomUser = webSocketList.chatRoomUser.get(this.wsData.data.articleuuid);
+
+    if (!chatRoom.has(this.wsData.data.useruuid)) {
+      // 添加数据
+      chatRoom.set(this.wsData.data.useruuid, this.websocket);
+      chatRoomUser.push(this.wsData.data);
+      // 覆盖数据
+      webSocketList.chatRoom.set(this.wsData.data.articleuuid, chatRoom);
+      webSocketList.chatRoomUser.set(this.wsData.data.articleuuid, chatRoomUser);
+
+      // 发送数据
+      for (const websocketItem of chatRoom.values()) {
+        websocketItem.send(JSON.stringify({
+          data: chatRoomUser,
+          callback: 'joinChatRoom'
+        }));
+      }
     }
   }
   // 聊天室 - 退出房间
   async closeChatRoomAction() {
-    if (webSocketList.chatRoom.has(this.wsData.data.useruuid)) {
-      webSocketList.chatRoom.delete(this.wsData.data.useruuid);
+    const chatRoom = webSocketList.chatRoom.get(this.wsData.data.articleuuid);
+    let chatRoomUser = webSocketList.chatRoomUser.get(this.wsData.data.articleuuid);
+
+    if (chatRoom.has(this.wsData.data.useruuid)) {
+      // 添加数据
+      chatRoom.delete(this.wsData.data.useruuid);
+      chatRoomUser = chatRoomUser.filter(item => {
+        return item.useruuid !== this.wsData.data.useruuid;
+      });
+      webSocketList.chatRoomUser.set(this.wsData.data.articleuuid, chatRoomUser);
+
+      // 发送数据
+      for (const websocketItem of chatRoom.values()) {
+        websocketItem.send(JSON.stringify({
+          data: chatRoomUser,
+          callback: 'closeChatRoom'
+        }));
+      }
     }
   }
   // 聊天室 -发送信息
   async sendChatRoomAction() {
-    for (const websocketItem of webSocketList.chatRoom.values()) {
+    const chatRoom = webSocketList.chatRoom.get(this.wsData.data.articleuuid);
+    for (const websocketItem of chatRoom.values()) {
       websocketItem.send(JSON.stringify(this.wsData.data));
     }
   }
